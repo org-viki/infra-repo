acccbf1f25006e0fe7140b20c3c486c58df9f1d2
viki32 <vk@gmail.com>
2026-08-30 22:29:11 +0530

    auth:true and oidc true

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "azurerm" {
     # use_cli = true 
     use_azuread_auth = true
-
     resource_group_name  = "rg-tfstate"
     storage_account_name = "sttfstatevikram001"
     container_name       = "bootstrap"
