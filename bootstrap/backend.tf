35f77d01e6237bfce9a3832a99a6f125e1c314a6
Vivek <vivekdandyan32@gmail.com>
2026-08-31 17:51:18 +0530

    cluster auto scale enabled

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "azurerm" {
     # use_cli = true 
     use_azuread_auth     = true
+    subscription_id      = "e2fc2850-80df-42a0-87f0-130f871a996a"
     resource_group_name  = "rg-tfstate"
     storage_account_name = "sttfstatevikram001"
     container_name       = "bootstrap"
